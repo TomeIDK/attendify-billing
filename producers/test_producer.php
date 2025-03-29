8e103c533903b23bb25c3c2e07d958d66bdfdfbb
tomeidk <cecepas2001@gmail.com>
2025-03-29 16:11:12 +0100

    dockerfile + compose setup tested, connect consumer+producer

--- producers/test_producer.php
+++ producers/test_producer.php
@@ -5,52 +5,86 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
 // Update credentials to match docker-compose.yml
-$connection = new AMQPStreamConnection(
-    'rabbitmq',    // host
-    5672,          // port
-    'guest',       // user (changed from 'fossbilling')
-    'guest'        // password (changed from 'fossbilling')
-);
-
+$connection = new AMQPStreamConnection('localhost', 5672, 'user', 'password', 'vhost');
 $channel = $connection->channel();
 
-// Declare the exchange
-$channel->exchange_declare('user_events', 'topic', false, true, false);
 
 // Updated user data with a valid email format
 $userData = [
-    'status' => 'active',
-    'group_id' => 1,  // You might need to adjust this based on your FOSSBilling groups
-    'email' => 'Bilal.belkasem@gmail.com',  // Updated email format
+    'id' => '12345', 
     'first_name' => 'Bilal',
     'last_name' => 'Belkasem',
-    'company' => 'souls inc.',
-    'address_1' => 'la nigrillo anarctito',
-    'address_2' => 'Bombardino crocodilo',
-    'city' => 'Columbus',
-    'state' => 'Ohio',
-    'country' => 'US',  // Country code
-    'postcode' => '170025',
-    'phone_cc' => '56',  // Country calling code for Netherlands
-    'phone' => '254876156',
-    'currency' => 'USD',
+    'date_of_birth' => '1990-01-01', // Example DOB
+    'phone_number' => '+3212345678',
+    'title' => 'Developer',
+    'email' => 'Bilal.belkasem@gmail.com',
     'password' => 'SecurePassword123!',
-    'password_confirm' => 'SecurePassword123!'  // Added password_confirm
+    'email_registered' => 'true',
+    'from_company' => 'false',
+    
+    'address' => [
+        'street' => 'la nigrillo anarctito',
+        'number' => '42',
+        'bus_number' => 'B',
+        'city' => 'Columbus',
+        'province' => 'Ohio',
+        'country' => 'US',
+        'postal_code' => '170025'
+    ],
+
+    'company' => [
+        'id' => 'COM001',
+        'name' => 'souls inc.',
+        'VAT_number' => 'BE987654321',
+        'address' => [
+            'street' => 'Corporate Avenue',
+            'number' => '10',
+            'city' => 'Ghent',
+            'province' => 'East Flanders',
+            'country' => 'Belgium',
+            'postal_code' => '9000'
+        ]
+    ]
 ];
 
 // Add debug output
-echo "Sending user data: " . json_encode($userData, JSON_PRETTY_PRINT) . "\n";
+echo "[*] Sending user data: " . json_encode($userData, JSON_PRETTY_PRINT) . "\n";
+
+function arrayToXml($data, $xml) {
+    foreach ($data as $key => $value) {
+        if (is_array($value)) {
+            $subnode = $xml->addChild($key);
+            arrayToXml($value, $subnode);
+        } else {
+            $xml->addChild($key, htmlspecialchars($value));
+        }
+    }
+}
+
+$xml = new SimpleXMLElement('<attendify></attendify>');
+$info = $xml->addChild('info');
+$info->addChild('sender', 'billing');
+$info->addChild('operation', 'create');
+
+
+$user = $xml->addChild('user');
+arrayToXml($userData, $user);
+
+// remove manually set fields
+unset($userData['sender'], $userData['operation']);
+
+$xmlString = $xml->asXML();
 
 // Convert to JSON
 $msg = new AMQPMessage(
-    json_encode($userData),
-    ['content_type' => 'application/json']
+    $xmlString,
+    ['content-type' => 'application/xml']
 );
 
 // Publish message
-$channel->basic_publish($msg, 'user_events', 'user.created');
+$channel->basic_publish($msg, 'user-management', 'user.register');
 
-echo "Message sent with user data\n";
+echo "[✔] Message sent with user data (XML format):\n$xmlString\n";
 
 $channel->close();
 $connection->close();
