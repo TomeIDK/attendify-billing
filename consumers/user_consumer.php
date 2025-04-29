ec43d4e8afb5a1acc49ef5e120e6c7d3cffbc3af
Giles Synaeve <giles.synaeve@student.ehb.be>
2025-04-29 13:59:21 +0200

    require once

--- consumers/user_consumer.php
+++ consumers/user_consumer.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/../parser.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
