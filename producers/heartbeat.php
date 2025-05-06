15192db248c91826f60bbf59a82a8f2b630dc0ac
tomeidk <cecepas2001@gmail.com>
2025-05-06 11:02:23 +0200

    update vendor path + log

--- producers/heartbeat.php
+++ producers/heartbeat.php
@@ -1,6 +1,6 @@
 <?php
-
-require_once('/app/producers/vendor/autoload.php');
+echo __DIR__;
+require_once __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/../parser.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
