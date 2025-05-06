8654c4e901a729de84fee1b9d2c0eb32f492987d
tomeidk <cecepas2001@gmail.com>
2025-05-06 12:57:02 +0200

    update path to files

--- consumers/start.php
+++ consumers/start.php
@@ -6,10 +6,10 @@ print_r(scandir(getcwd()));
 // > /dev/null 2>&1 & 
 // ensure processes run in the background (Linux)
 echo "Starting heartbeat.php...\n";
-exec('php heartbeat.php > /dev/null 2>&1 &');
+exec('php consumers/heartbeat.php > /dev/null 2>&1 &');
 echo "heartbeat.php has started.\n";
 
 // run main service in the foreground
 echo "Starting user_consumer.php...\n";
-passthru('php user_consumer.php');
+passthru('php consumers/user_consumer.php');
 echo "user_consumer.php has started.\n";
--- producers/start.php
+++ producers/start.php
@@ -6,10 +6,10 @@ print_r(scandir(getcwd()));
 // > /dev/null 2>&1 & 
 // ensure processes run in the background (Linux)
 echo "Starting heartbeat.php...\n";
-exec('php heartbeat.php > /dev/null 2>&1 &');
+exec('php producers/heartbeat.php > /dev/null 2>&1 &');
 echo "heartbeat.php has started.\n";
 
 // run main service in the foreground
 echo "Starting user_producer.php...\n";
-passthru('php user_producer.php');
+passthru('php producers/user_producer.php');
 echo "user_producer.php has started.\n";
