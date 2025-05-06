82660d6df7334a67231d205d49c05b3d3d5ead64
tomeidk <cecepas2001@gmail.com>
2025-05-06 10:49:39 +0200

    update path to vendor

--- producers/user_producer.php
+++ producers/user_producer.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once './vendor/autoload.php';
 require __DIR__ . '/../parser.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
