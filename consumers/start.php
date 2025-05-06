68f545fbf65e71d34367f549eb8469d257d41c7e
tomeidk <cecepas2001@gmail.com>
2025-05-06 10:41:26 +0200

    custom services heartbeat

---
+++ consumers/start.php
@@ -0,0 +1,12 @@
+<?php
+
+// run main service in the foreground
+echo "Starting user_consumer.php...\n";
+passthru('php user_consumer.php');
+echo "user_consumer.php has started.\n";
+
+// > /dev/null 2>&1 & 
+// ensure processes run in the background (Linux)
+echo "Starting heartbeat.php...\n";
+exec('php heartbeat.php > /dev/null 2>&1 &');
+echo "heartbeat.php has started.\n";
